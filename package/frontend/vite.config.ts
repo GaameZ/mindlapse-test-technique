81fd062d4ae8c7ff1b322aa7e13ba2cd8405ad27
Alexy Samyn <alexy59430@gmail.com>
2026-02-09 15:05:32 +0100

    feat(frontend): add .gitignore file and update Vite config for environment directory

--- package/frontend/vite.config.ts
+++ package/frontend/vite.config.ts
@@ -2,8 +2,11 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import path from "path";
 
 export default defineConfig({
+  envDir: path.resolve(__dirname, "../.."),
+  
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
   server: {
     port: 5173,
